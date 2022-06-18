91e05a44710d6b593c09c14953efdc8ad5106a7f
莫肇聪 <mozhaocong@qq.com>
2022-06-18 15:34:38 +0800

    feature: 更新

--- layout/modules/routerTagList/index.tsx
+++ layout/modules/routerTagList/index.tsx
@@ -15,6 +15,10 @@ export default defineComponent({
 			console.log('erpLayoutModule.routerTagList', erpLayoutModule.routerTagList)
 			return erpLayoutModule.routerTagList || []
 		})
+		setInterval(() => {
+			console.log('setInterval')
+			console.log(erpLayoutModule.routerTagList)
+		}, 1000)
 
 		const route = useRoute()
 		const router = useRouter()
