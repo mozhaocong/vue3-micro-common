2ae6d45ee2066de37d5d6be4d027ae21cd014179
莫肇聪 <mozhaocong@qq.com>
2022-06-20 18:27:39 +0800

    feature: 更新

--- api/erp/index.ts
+++ api/erp/index.ts
@@ -1,6 +1,5 @@
 let erpUrl = 'http://erp_test.admin.htwig.com'
 let apiUrl = 'http://crm_test.htwig.com'
-console.log('import.meta.env.VITE_ENV', import.meta.env.VITE_ENV)
 switch (import.meta?.env?.VITE_ENV) {
 	case 'test':
 		erpUrl = 'http://erp_test.admin.htwig.com'
