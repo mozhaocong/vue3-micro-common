0433a7339e85250abb22ff51d7e628179405d5fe
莫肇聪 <mozhaocong@qq.com>
2022-06-21 14:35:23 +0800

    feature: vuex替换

--- store/index.ts
+++ store/index.ts
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-import { erpLayoutModule } from '@/store/modules/erp/public/layout'
+import erpLayoutModule from '@/store/modules/erp/public/layout'
 export default createStore({
 	modules: {
 		erpLayout: erpLayoutModule,
