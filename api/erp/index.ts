1afc74d5ace799a306a14c197e697e08ecea05f6
莫肇聪 <mozhaocong@qq.com>
2022-06-30 13:55:24 +0800

    feature: 地址修改

--- api/erp/index.ts
+++ api/erp/index.ts
@@ -4,7 +4,7 @@ try {
 	switch (import.meta.env.VITE_ENV) {
 		case 'test':
 			erpUrl = 'http://erp_test.admin.htwig.com'
-			erpUrl = 'https://erp.admin.htwig.com'
+			apiUrl = 'http://crm_test.htwig.com'
 			break
 		case 'prod':
 			erpUrl = 'https://erp.admin.htwig.com'
