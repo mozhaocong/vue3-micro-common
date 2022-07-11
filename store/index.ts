3b3371179be163c4bc6d480175b6b9447bac2a60
莫肇聪 <mozhaocong@qq.com>
2022-07-11 18:34:25 +0800

    feature: 更新

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import erpLayoutModule from '@/store/modules/erp/public/layout'
 import erpLoginModule from '@/store/modules/erp/public/login'
+import basicDataModule from '@/store/modules/erp/business/index'
 export default createStore({
 	modules: {
 		erpLayout: erpLayoutModule,
 		erpLogin: erpLoginModule,
+		basicData: basicDataModule,
 	},
 })
