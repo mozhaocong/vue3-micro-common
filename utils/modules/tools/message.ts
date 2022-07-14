b3517a529baf2a164177ec1f81434f8cb9793176
mozhaocong <18825060695@163.com>
2022-07-15 00:35:25 +0800

    feature: 更新

--- utils/modules/tools/message.ts
+++ utils/modules/tools/message.ts
@@ -3,11 +3,12 @@ import { message } from 'ant-design-vue'
 import { throttle } from '@/utils'
 
 export function curryMessage(type: string, data: string) {
+	console.log(type, data)
 	if (type === 'error') {
 		message.error(data)
-	} else if (type === 'warning ') {
+	} else if (type === 'warning') {
 		message.warning(data)
-	} else if (type === 'success ') {
+	} else if (type === 'success') {
 		message.success(data)
 	}
 }
