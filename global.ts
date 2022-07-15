74ffe97fe9dd8a40df4e009e00829a468a2ab588
莫肇聪 <mozhaocong@qq.com>
2022-07-15 18:12:27 +0800

    feature: 更新

--- global.ts
+++ global.ts
@@ -1,9 +1,11 @@
 import { ColumnGroupType, ColumnsType, ColumnType } from 'ant-design-vue/lib/table/interface'
 interface formTableColumns extends ColumnType<any> {
 	row?: FormRowArray
+	display?: () => boolean | boolean
 }
 interface formTableColumnsGroup extends ColumnGroupType<any> {
 	row?: FormRowArray
+	display?: () => boolean | boolean
 }
 
 declare global {
