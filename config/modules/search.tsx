d329c71bde672f6b8af54610e25684a194d66dd0
陈江洋 <1584227583@qq.com>
2022-07-20 15:28:50 +0800

    feature: 默认时间替换修正

--- config/modules/search.tsx
+++ config/modules/search.tsx
@@ -3,8 +3,8 @@ import dayjs from 'dayjs'
 import { FormConfig, RRangePicker } from '@/components'
 
 export const searchModelDefData = {
-	start_created_at: dayjs().format(valueFormat),
-	end_created_at: dayjs().subtract(1, 'M').format(valueFormat),
+	end_created_at: dayjs().format(valueFormat),
+	start_created_at: dayjs().subtract(1, 'M').format(valueFormat),
 	is_simple: 1,
 }
 
