e28bd21d4dee963a6cab09b96558e73f932dbe4d
陈江洋 <1584227583@qq.com>
2022-07-28 18:27:00 +0800

    feature: 新增oss正式测试

--- api/erp/index.ts
+++ api/erp/index.ts
@@ -1,18 +1,21 @@
 let erpUrl = 'http://erp_test.admin.htwig.com'
 let apiUrl = 'http://crm_test.htwig.com'
+let ossUrl = 'http://oss-dev.htwig.com'
 try {
 	switch (import.meta.env.VITE_ENV) {
 		case 'test':
 			erpUrl = 'http://erp_test.admin.htwig.com'
 			apiUrl = 'http://crm_test.htwig.com'
+			ossUrl = 'http://oss-dev.htwig.com'
 			break
 		case 'prod':
 			erpUrl = 'https://erp.admin.htwig.com'
 			apiUrl = 'https://portal.admin.htwig.com'
+			ossUrl = 'http://oss.htwig.com'
 			break
 	}
 } catch (e) {
 	console.log(e)
 }
 
-export { apiUrl, erpUrl }
+export { apiUrl, erpUrl, ossUrl }
