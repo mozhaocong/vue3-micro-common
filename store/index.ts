e48f1b20c51ff1438ec5f3a1f1052ea759d5e017
mozhaocong <>
2022-09-08 18:12:56 +0800

    feature: ts声明开发

--- store/index.ts
+++ store/index.ts
@@ -3,11 +3,31 @@ import erpLayoutModule from '@/store/modules/erp/public/layout'
 import erpLoginModule from '@/store/modules/erp/public/login'
 import basicDataModule from '@/store/modules/erp/business/index'
 import shippingDataModule from '@/store/modules/erp/business/shipping'
+
+const modules = {
+	erpLayout: erpLayoutModule,
+	erpLogin: erpLoginModule,
+	basicData: basicDataModule,
+	shippingData: shippingDataModule,
+}
+
 export default createStore({
-	modules: {
-		erpLayout: erpLayoutModule,
-		erpLogin: erpLoginModule,
-		basicData: basicDataModule,
-		shippingData: shippingDataModule,
-	},
+	modules,
 })
+
+type vuexModulesType = typeof modules
+
+type test = keyof vuexModulesType
+
+type getCommitName<T> = T extends { mutations: infer G } ? G : never
+type testData = {
+	[K in keyof vuexModulesType]: getCommitName<vuexModulesType[K]>
+}
+
+type storeType = {
+	getters?: ObjectMap
+	commit?: ObjectMap
+	dispatch?: ObjectMap
+}
+
+const store: storeType = {}
